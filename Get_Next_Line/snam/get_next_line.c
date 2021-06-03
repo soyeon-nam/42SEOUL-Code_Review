b5bc96441b89ea9e68ba4d79adf8af48dde5a889
24siefil <24siefil>
2021-06-03 18:20:32 +0900

    backup

--- Get_Next_Line/snam/get_next_line.c
+++ Get_Next_Line/snam/get_next_line.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snam <snam@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sshin <sshin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 06:42:38 by snam              #+#    #+#             */
-/*   Updated: 2021/06/03 14:47:04 by snam             ###   ########.fr       */
+/*   Updated: 2021/06/03 18:20:13 by sshin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+// &backup[fd]로 들어온것을 fd_backup으로 선언하는것 좋은것 같다.
+// 나는 char **backup으로 받았는데 fd_backup으로 구분해주는것이 더 좋은듯. -sshin
 int			concat_old_backup(char **fd_backup, char **line, char *buf)
 {
 	int				flag;
@@ -30,7 +31,7 @@ int			concat_old_backup(char **fd_backup, char **line, char *buf)
 	}
 	else
 	{
-		if (!(*line = (char *)malloc(1)))
+		if (!(*line = (char *)malloc(1))) // <- line에 할당할 준비 한칸 만들고 0 넣음
 		{
 			free(buf);
 			return (-1);
@@ -94,12 +95,23 @@ int			read_file(char **line, char **fd_backup, char **buf, int fd)
 	return (0);
 }
 
+// libft 함수(표준이거나 빈번하게 사용되어 누구나 그 기능을 알고있는 함수)를 최대한 활용했으면
+// 좀 더 이해하기 쉬운 코드가 되었을것 같음.
+// 함수의 배치는 논리의 흐름에 따라 위에서 아래로 읽도록 배치하는것이 좋다고 알고있다.
+// gnl 함수를 최상단에 배치하고 concat_old_backup, read_file, concat_new_buf,
+// generate_ret_line 순으로 배치하는것이 좋지 않았을까. -sshin
 int			get_next_line(int fd, char **line)
 {
+	// 주관이긴 한데 변수 선언을 아래와 같이 변수 사용 순서에 따라 하는것이 가독성에 좋다고 생각함.
+	// char			*buf;
+	// int			ret;
+	// static char	*fd_backup[OPEN_MAX]; -sshin
 	static char		*fd_backup[OPEN_MAX];
 	char			*buf;
 	int				ret;
 
+	// fd 값에 대한 예외처리를 아래와 같이 하는것 좋은것 같다!
+	// 특히 두번째 인자를 따로 변수를 선언하지 않고 빈문자로 처리해서 임의의 주솟값을 가지고 오는것이 인상적 -sshin
 	if ((read(fd, "", 0) == -1) || !line || BUFFER_SIZE <= 0)
 		return (-1);
 	if (!(buf = (char *)malloc(BUFFER_SIZE + 1)))
